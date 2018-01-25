0f180992f30e8cc40b03d1d34d9680ed62eb0f37
kotpsot <zygmunt.justyna@gmail.com>
2018-01-25 15:21:34 +0000

    split release_bike method into two, add empty and full methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -25,7 +25,7 @@ describe DockingStation do
   describe '#dock' do
     it 'raises error when full' do
       20.times { subject.dock(Bike.new) }
-      expect { subject.dock Bike.new }.to raise_error "Docking station full"
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
 
