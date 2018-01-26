c764ae9fe53cad43f3f74a41e20e4cdb5777ac94
Matt Burstein <j.matthew.burstein@gmail.com>
2018-01-26 14:28:06 +0000

    Replace Bike.new with double in docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,8 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike}
 
   it 'releases working bikes' do
-    bike = Bike.new
+
+    bike = double("a bike")
     subject.dock(bike)
     expect(subject.release_bike).to be_instance_of(Bike)
   end
@@ -13,14 +14,16 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it 'docks bike' do
-     bike = Bike.new
-     expect(subject.dock(bike)).to eq [bike]
+    bike = double("a bike")
+    # bike = Bike.new # double here
+    expect(subject.dock(bike)).to eq [bike]
    end
 
   describe '#dock' do
     it 'raises error when full' do
-      subject.capacity.times { subject.dock Bike.new }
-      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+      bike = double("a bike")
+      subject.capacity.times { subject.dock bike} # double here
+      expect { subject.dock bike }.to raise_error 'Docking station full' # double here
     end
   end
 
@@ -29,18 +32,20 @@ describe DockingStation do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
     it 'doesnt release broken bikes' do
-      bike = Bike.new(false)
+      bike = double("a bike")
+      # bike = Bike.new(false) # double here
       station = DockingStation.new
       station.dock(bike)
-      expect(station.release_bike).to eq nil #doesn't release bike as there is only one bike and it is broken 
+      expect(station.release_bike).to eq nil #doesn't release bike as there is only one bike and it is broken
     end
   end
 
   describe 'initialization' do
     # subject { DockingStation.new }
-    let(:bike) { Bike.new }
+    # let(:bike) { Bike.new } # double here
     # bike = Bike.new
     it 'defaults capacity' do
+      bike = double("a bike")
       described_class::DEFAULT_CAPACITY.times do
         subject.dock(bike)
       end
